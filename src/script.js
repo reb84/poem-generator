71b491224d158ac5b4bf91ead6abb44b5f7df669
reb84 <152401972+reb84@users.noreply.github.com>
2024-01-20 18:40:49 +0000

    api javascript applied

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
 function generatePoem(event) {
   event.preventDefault();
+  let instructionsInput = document.querySelector("#submit-instructions");
+  let apiKey = "22de0057ea42aa649cbcof0e3b1te784";
+  let context =
+    "You are a Poem expert and love to write short poems. You mission is to generate poem with a maximum of 10 lines in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionsInput.value}...</div>`;
+
+  axios.get(apiUrl).then(typePoem);
+}
+
+function typePoem(response) {
   new Typewriter("#poem", {
-    strings: `My heart’s in the Highlands, my heart is not here,
-My heart’s in the Highlands, a-chasing the deer;
-Chasing the wild-deer, and following the roe,
-My heart’s in the Highlands, wherever I go…`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
     cursor: "",
